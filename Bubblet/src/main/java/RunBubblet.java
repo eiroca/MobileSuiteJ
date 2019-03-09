4108c156c854ec527a0ac5e325d3e270df5e5ed5
eiroca <39483731+eiroca@users.noreply.github.com>
2019-03-09 21:42:33 +0100

    Bubblet conversion

---
+++ Bubblet/src/main/java/RunBubblet.java
@@ -0,0 +1,10 @@
+import org.recompile.freej2me.J2MEHost;
+import net.eiroca.j2me.bubblet.Bubblet;
+
+public class RunBubblet {
+
+  public static void main(String[] args) {
+    J2MEHost host = new J2MEHost(Bubblet.class);
+  }
+
+}
